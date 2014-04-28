2ae947c9c8875b35a3541270820eef21a27865a4
ALF <angheloalf95@gmail.com>
2014-04-28 08:42:44 -0400

    Se le agrega sonidos a background
    Background se le agrega musica de fondo (GourmetRace.ogg), sonidos al
    caminar (wood1.ogg) y cuando se clickea (hit1.ogg).
    Ademas se pone nuestro background (WIP)

--- main - background music.py
+++ main - background music.py
@@ -1,15 +1,16 @@
-import pygame, sys, os
+import pygame, sys, os, random
 from pygame.locals import *
 pygame.init() #inicia pygame
 #inicia la ventana y su nombre
-dimx=500;dimy=500 #dimensiones de la ventana
-spritesheet = pygame.image.load(os.path.join("media","Arq44x76.png"))
-ven=pygame.display.set_mode((dimx,dimy),0,32)
-spritesheet.convert()
+dimx=1280;dimy=720 #dimensiones de la ventana
+spritesheet = pygame.image.load(os.path.join("Arq44x76.png"))
+warning=pygame.image.load(os.path.join("warn.png"))
+ven=pygame.display.set_mode((dimx,dimy),pygame.FULLSCREEN)
+spritesheet.convert();warning.convert()
 pygame.display.set_caption("Nombre del juego")
-background = pygame.Surface((ven.get_size()))
+background = pygame.image.load(os.path.join("media","background2.png"))
 backgroundrect = background.get_rect()
-background.fill((255,255,255)) # relleno de fondo a cambiar por imagen
+#background.fill((255,255,255)) # relleno de fondo a cambiar por imagen
 background = background.convert()  
 ven.blit(background,(0,0))
 
@@ -20,8 +21,11 @@ pygame.mixer.init(frequency=22050, size=-16, channels=8, buffer=4096) ##iniciado
 sound=pygame.mixer.Sound(os.path.join(data_dir, "overworld1.ogg"))      ##musica de fondo(test)
 paso=pygame.mixer.Sound(os.path.join(data_dir, "wood1.ogg"))         ##sonido cuando caminas
 GR=pygame.mixer.Sound(os.path.join(data_dir, "GourmetRace.ogg"))     ##prueba useless :C
-paso.set_volume(0.4)       ##volumen de los pasos
-pygame.mixer.Sound.play(sound) ##se reproduce la musica de fondo
+GR.set_volume(0.3)       ##volumen de GR
+#paso.set_volume(1.0)       ##volumen de los pasos
+pygame.mixer.Sound.play(GR) ##se reproduce la musica de fondo
+click1=pygame.mixer.Sound(os.path.join(data_dir, "hit1.ogg"))
+
 
 arq=[]
 for alf in range(1,11,1): # recorrer 10 elementos para arq
@@ -45,13 +49,13 @@ picnr = 0
 posx=300;posy=300 #posiciones de img
 der= False;aba=False;izq=False;arr= False # variables de movimiento en falso
 standf=True;standb=False #detenido hacia adelante o atras
-
 while mainloop:
     milliseconds = clock.tick(FPS)  # milisec despues del ultimo frame
     seconds = milliseconds / 1000.0 # seconds q pasaron del utimo frame
     playtime += seconds
     cycletime += seconds
     mypicture = arq[picnr]
+    mouspos=pygame.mouse.get_pos()
     #condicionales de stand y direccion
     if (der and izq and arr and aba) == False:
         standf=True
@@ -60,14 +64,14 @@ while mainloop:
     if cycletime > interval:
         if standf:
             picnr=0
-            ven.blit(background.subsurface((300,300,48,76)),(posx,posy)) #limpia imagen anterior
+            ven.blit(background.subsurface((posx,posy,48,76)),(posx,posy)) #limpia imagen anterior
             ven.blit(mypicture, (posx,posy))
         if standb:
             picnr=3
-            ven.blit(background.subsurface((300,300,48,76)),(posx,posy)) 
+            ven.blit(background.subsurface((posx,posy,48,76)),(posx,posy)) 
             ven.blit(mypicture, (posx,posy))                
         if (der or izq or aba)and (not arr):
-            ven.blit(background.subsurface((300,300,48,76)),(posx,posy)) 
+            ven.blit(background.subsurface((posx,posy,48,76)),(posx,posy)) 
             ven.blit(mypicture, (posx,posy))
             pygame.mixer.Sound.play(paso) #reproduce el sonido de los pasos cuando caminas
             picnr += 1
@@ -85,16 +89,14 @@ while mainloop:
         if arr:
             if picnr!=5:
                 picnr=4
-            ven.blit(background.subsurface((300,300,48,76)),(posx,posy)) 
+            ven.blit(background.subsurface((posx,posy,48,76)),(posx,posy)) 
             ven.blit(mypicture, (posx,posy))
             picnr += 1
-            pygame.mixer.Sound.play(paso)
+            pygame.mixer.Sound.play(paso) #reproduce el sonido de los pasos cuando caminas
             if picnr >= 6:
                 picnr = 4
         cycletime = 0
 
-                
-            
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             mainloop = False # X de la ventana
@@ -106,25 +108,28 @@ while mainloop:
             if event.key == pygame.K_a: izq = True
             if event.key == pygame.K_w: arr= True
             if event.key == pygame.K_s: aba= True
-
         elif event.type == pygame.KEYUP:
             #if soltar tecla
             if event.key == pygame.K_d: der=False
             if event.key == pygame.K_a: izq = False
             if event.key == pygame.K_w: arr= False
             if event.key == pygame.K_s: aba= False
-
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            if pygame.mouse.get_pressed()[0]==True:
+               if (522<mouspos[0]<788)and (284<mouspos[1]<320):
+                  ven.blit(warning,(random.randint(0,dimx),random.randint(0,dimy)))
+                  pygame.mixer.Sound.play(click1)
     if der and posx<(dimx-48):
-        ven.blit(background.subsurface((0,0,48,76)),(posx,posy))#limpia y redibuja
+        ven.blit(background.subsurface((posx,posy,48,76)),(posx,posy))#limpia y redibuja
         posx +=3;ven.blit(mypicture, (posx,posy))
     if izq and posx > 0:
-        ven.blit(background.subsurface((0,0,48,76)),(posx,posy))
+        ven.blit(background.subsurface((posx,posy,48,76)),(posx,posy))
         posx -=3;ven.blit(mypicture, (posx,posy))
     if arr and posy > 0:
-        ven.blit(background.subsurface((0,0,48,76)),(posx,posy))
+        ven.blit(background.subsurface((posx,posy,48,76)),(posx,posy))
         posy -=3;ven.blit(mypicture, (posx,posy))
     if aba and posy<(dimy-76):
-        ven.blit(background.subsurface((0,0,48,76)),(posx,posy))
+        ven.blit(background.subsurface((posx,posy,48,76)),(posx,posy))
         posy +=3;ven.blit(mypicture, (posx,posy))
 
     pygame.display.flip()
